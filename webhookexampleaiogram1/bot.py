177ab049a1f8c1cef76f8e269e4308718c3dcc20
andrei <dorofeichik9723@gmail.com>
2022-07-23 14:53:11 +0300

    2 commit

--- webhookexampleaiogram1/bot.py
+++ webhookexampleaiogram1/bot.py
@@ -1,52 +1,48 @@
-import asyncio
 import logging
 import os
-
-from aiogram import Bot, types
+from aiogram import Bot
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils.executor import start_webhook
+from aiogram import Bot, types
 
-API_TOKEN = os.getenv("BOT_TOKEN")
-HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
+
+TOKEN = os.getenv('BOT_TOKEN')
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
 
 # webhook settings
-WEBHOOK_HOST = f"https://{HEROKU_APP_NAME}.herokuapp.com"
-WEBHOOK_PATH = f"/webhook/{API_TOKEN}"
-WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
+WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
+WEBHOOK_PATH = f'/webhook/{TOKEN}'
+WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
 
 # webserver settings
-WEBAPP_HOST = "0.0.0.0"
-WEBAPP_PORT = 8000
+WEBAPP_HOST = '0.0.0.0'
+WEBAPP_PORT = os.getenv('PORT', default=8000)
 
-logging.basicConfig(level=logging.INFO)
 
-loop = asyncio.get_event_loop()
-bot = Bot(token=API_TOKEN, loop=loop)
-dp = Dispatcher(bot)
+async def on_startup(dispatcher):
+    await bot.set_webhook(WEBHOOK_URL, drop_pending_updates=True)
+
+
+async def on_shutdown(dispatcher):
+    await bot.delete_webhook()
 
 
 @dp.message_handler()
 async def echo(message: types.Message):
-    await bot.send_message(message.chat.id, message.text)
+    await message.answer(message.text)
 
 
-async def on_startup(dp):
-    await bot.set_webhook(WEBHOOK_URL)
-    # insert code here to run it after start
-
-
-async def on_shutdown(dp):
-    await bot.delete_webhook()
-    pass
-
-
-if __name__ == "__main__":
+ if __name__ == '__main__':
+    logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
         webhook_path=WEBHOOK_PATH,
+        skip_updates=True,
         on_startup=on_startup,
         on_shutdown=on_shutdown,
-        skip_updates=True,
         host=WEBAPP_HOST,
         port=WEBAPP_PORT,
     )
