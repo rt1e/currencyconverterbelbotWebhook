0a2f08dac0bbc4a3abccf28be55447e422eab561
andrei <dorofeichik9723@gmail.com>
2022-07-23 18:53:04 +0300

    fix database connection

--- webhookexampleaiogram1/db.py
+++ webhookexampleaiogram1/db.py
@@ -1,18 +1,14 @@
 from databases import Database
 
 
-database = Database("sqlite:///bot.db")
-
-
-async def create_table(database):
-    await database.connect()
-    query = """CREATE TABLE messages (
+database = Database("sqlite+aiosqlite:///bot.db")
+await database.connect()
+query = """CREATE TABLE messages (
                                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                                 telegram_id INTEGER NOT NULL,
                                 text text NOT NULL
                                 );"""
-    await database.execute(query=query)
-    return database
+await database.execute(query=query)
 
 
 database = create_table(database)
