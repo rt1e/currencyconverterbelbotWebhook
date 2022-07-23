c71f61224358b2cd4ff875172aa2831d73ecc609
andrei <dorofeichik9723@gmail.com>
2022-07-23 14:56:38 +0300

    fixed indentation

--- webhookexampleaiogram1/bot.py
+++ webhookexampleaiogram1/bot.py
@@ -6,20 +6,20 @@ from aiogram.utils.executor import start_webhook
 from aiogram import Bot, types
 
 
-TOKEN = os.getenv('BOT_TOKEN')
+TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-HEROKU_APP_NAME = os.getenv('HEROKU_APP_NAME')
+HEROKU_APP_NAME = os.getenv("HEROKU_APP_NAME")
 
 # webhook settings
-WEBHOOK_HOST = f'https://{HEROKU_APP_NAME}.herokuapp.com'
-WEBHOOK_PATH = f'/webhook/{TOKEN}'
-WEBHOOK_URL = f'{WEBHOOK_HOST}{WEBHOOK_PATH}'
+WEBHOOK_HOST = f"https://{HEROKU_APP_NAME}.herokuapp.com"
+WEBHOOK_PATH = f"/webhook/{TOKEN}"
+WEBHOOK_URL = f"{WEBHOOK_HOST}{WEBHOOK_PATH}"
 
 # webserver settings
-WEBAPP_HOST = '0.0.0.0'
-WEBAPP_PORT = os.getenv('PORT', default=8000)
+WEBAPP_HOST = "0.0.0.0"
+WEBAPP_PORT = os.getenv("PORT", default=8000)
 
 
 async def on_startup(dispatcher):
@@ -35,7 +35,7 @@ async def echo(message: types.Message):
     await message.answer(message.text)
 
 
- if __name__ == '__main__':
+if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     start_webhook(
         dispatcher=dp,
